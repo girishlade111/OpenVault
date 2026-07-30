2b014f3ffc6a0ae261518db80ac7a99c8eec2a45
Girish Lade <girishlade111@gmail.com>
2026-07-30 03:33:15 -0700

    chore: setup github actions release workflow, update build configs

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function createWindow() {
     width: 1280,
     height: 800,
     titleBarStyle: 'hiddenInset',
+    icon: path.join(__dirname, 'build/icon.ico'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
